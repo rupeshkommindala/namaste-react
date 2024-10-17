f028f31edef076955cd5d4820e08348b158bdc62
Rupesh Yadav <rupeshyadav@Rupeshs-MacBook-Air.local>
2024-10-17 10:18:21 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
+    React.createElement("h2", {}, "Learning by Rupesh K"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "Hi"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 ]);
