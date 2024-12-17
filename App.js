eb7e7ba063a23066c9ca511e39dba0db7e38509e
Rupesh Yadav <rupeshyadav@Rupeshs-MacBook-Air.local>
2024-12-17 19:46:08 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-/**
- * <div id="parent">
- *  <div id="child">
- *      <h1>I'm an h1 tag</h1>
- *      <h2>I'm an h2 tag</h2>
- *  </div>
- *  <div id="child2">
- *      <h1>I'm an h1 tag</h1>
- *      <h2>I'm an h2 tag</h2>
- *  </div>
- * </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "Namaste React🚀"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -21,6 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 ]);
-console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
