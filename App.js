bbc36bd12329e49dc232f3101326c19111fa6cc3
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-08-12 07:10:53 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,16 +1,3 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm an h1 tag</h1>
- *          <h2>I'm an h2 tag</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>I'm an h1 tag</h1>
- *          <h2>I'm an h2 tag</h2>
- *      </div>
- * </div>
- */
-
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
@@ -21,6 +8,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 ]);
-console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
