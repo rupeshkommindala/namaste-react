816b9c8c34e0737fd0aaa7f11801aa97ab0f78d1
Rupesh Yadav <rupeshkommindala@gmail.com>
2025-11-08 08:14:14 +0530

    EP-01 | Inception

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /**
  * <div id="parent">
- *      <div id="child">
+ *      <div id="child1">
  *          <h1>I'm an h1 tag</h1>
  *          <h2>I'm an h2 tag</h2>
  *      </div>
@@ -9,10 +9,12 @@
  *          <h2>I'm an h2 tag</h2>
  *      </div>
  * </div>
+ *
+ * ReactElement(Object)=>HTML(Browser Understands)
  */
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
